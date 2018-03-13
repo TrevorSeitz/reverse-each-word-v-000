b22aa6bcb5676007d9c7bf2122d0c4c3dcc111f6
Trevor Seitz <trevinla@gmail.com>
2018-03-13 15:15:39 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,11 +1,6 @@
 def reverse_each_word(sentence)
-  reverse_array = []
   sentence_array = sentence.split(" ")
-  # sentence_array.each do |word|
-  #   reverse_array << word.reverse
-  # end
   sentence_array.collect {|word| word.reverse!}
-  # reverse_sentence = reverse_array.join(" ")
   reverse_sentence = sentence_array.join(" ")
   reverse_sentence
 end
