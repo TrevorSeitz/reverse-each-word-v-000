f1013c6775a989298f254564a60b238506c76390
Trevor Seitz <trevinla@gmail.com>
2018-03-13 15:09:47 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,9 +1,11 @@
 def reverse_each_word(sentence)
   reverse_array = []
   sentence_array = sentence.split(" ")
-  sentence_array.each do |word|
-    reverse_array << word.reverse
-  end
-  reverse_sentence = reverse_array.join(" ")
+  # sentence_array.each do |word|
+  #   reverse_array << word.reverse
+  # end
+  sentence_array.collect.reverse
+  # reverse_sentence = reverse_array.join(" ")
+  reverse_sentence = sentence_array.join(" ")
   reverse_sentence
 end
